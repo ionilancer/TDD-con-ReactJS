cb18805efae89dc6900bca3de6141c0b09699a4b
ionilancer <ionilancer@gmail.com>
2022-10-31 06:40:56 +0100

    test examples

---
+++ jest-intro/src/__tests__/example_async.test.ts
@@ -0,0 +1,25 @@
+//callbacks
+const asyncCallback = (cb: Function) => {
+  setTimeout(() => {
+    cb(true);
+  }, 1000);
+};
+
+//promises
+const asyncPromise = () => new Promise((resolve) => resolve(true));
+
+describe("async code", () => {
+  test("example of async with callback", (done) => {
+    asyncCallback((result: boolean) => {
+      expect(result).toBe(true);
+      done();
+    });
+  });
+  test("example of async with promises", () => {
+    return asyncPromise().then((result) => expect(result).toBe(true));
+  });
+  test("example of async with async/await", async() => {
+    const result = await asyncPromise();
+    expect(result).toBe(true);
+  });
+});
---
+++ jest-intro/src/__tests__/example_globals.test.ts
@@ -0,0 +1,11 @@
+describe("group for test cases", () => {
+  test("description", () => {
+    expect(true).toBe(true);
+  });
+  test("description 2", () => {
+    expect(true).toBe(true);
+  });
+  test("description 3", () => {
+    expect(true).toBe(true);
+  });
+});
